c64f61aa7b59b608cea831c34a5a8bf330b6a727
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2023-08-25 15:41:47 +0400

    add images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,6 +25,11 @@
                     handleClick={handleScroll}
                 />
             </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                </div>
+            </div>
         </div>
       )
     }
